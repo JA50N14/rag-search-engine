763f26389e27bf10498bee90eea9bccc779dcd5a
JA50N14 <macfarlane_j@hotmail.com>
2025-11-07 09:54:56 -0700

    completed CH2: TF-IDF

--- cli/lib/keyword_search.py
+++ cli/lib/keyword_search.py
@@ -1,20 +1,119 @@
 import string
 import os
-from .search_utils import DEFAULT_SEARCH_LIMIT, load_movies, load_stopwords
+import pickle
+import math
+from collections  import defaultdict, Counter
+
 from nltk.stem import PorterStemmer
 
+from .search_utils import DEFAULT_SEARCH_LIMIT, CACHE_DIR, load_movies, load_stopwords
+
+class InvertedIndex:
+    def __init__(self) -> None:
+        self.index = defaultdict(set)
+        self.docmap: dict[int, dict] = {}
+        self.term_frequencies = defaultdict(Counter)
+        self.index_path = os.path.join(CACHE_DIR, "index.pkl")
+        self.docmap_path = os.path.join(CACHE_DIR, "docmap.pkl")
+        self.tf_path = os.path.join(CACHE_DIR, "term_frequencies.pkl")
+
+    def load(self) -> None:
+        with open(self.index_path, "rb") as f:
+            self.index = pickle.load(f)
+
+        with open(self.docmap_path, "rb") as f:
+            self.docmap = pickle.load(f)
+
+        with open(self.tf_path, "rb") as f:
+            self.term_frequencies = pickle.load(f)
+    
+    def build(self) -> None:
+        movies = load_movies()
+        for movie in movies:
+            doc_id = movie["id"]
+            doc_description = f"{movie["title"]} {movie["description"]}"
+            self.docmap[doc_id] = movie
+            self.__add_document(doc_id, doc_description)
+            
+    def save(self):
+        os.makedirs(CACHE_DIR, exist_ok=True)
+        with open(self.index_path, "wb") as f:   
+            pickle.dump(self.index, f)
+        
+        with open(self.docmap_path, "wb") as f:
+            pickle.dump(self.docmap, f)
+
+        with open(self.tf_path, "wb") as f:
+            pickle.dump(self.term_frequencies, f)
+
+    def get_documents(self, term: str) -> list[int]:
+        term = term.lower()
+        doc_ids = self.index.get(term, set())
+        return sorted(list(doc_ids))
+    
+    def get_tf(self, doc_id: int, term: str) -> int:        
+        tokens = tokenize_text(term)
+        if len(tokens) != 1:
+            raise ValueError("term must be a single token")
+        token = tokens[0]
+        return self.term_frequencies[doc_id][token]
+
+    def get_idf(self, term: str) -> float:
+        tokens = tokenize_text(term)
+        if len(tokens) != 1:
+            raise ValueError("term must be a single token")
+        token = tokens[0]
+        term_doc_count = len(self.index[token])
+        doc_count = len(self.docmap)
+        return math.log((doc_count + 1)  / (term_doc_count + 1))
+
+    def get_tf_idf(self, doc_id: int, term: str) -> float:
+        tf = self.get_tf(doc_id, term)
+        idf = self.get_idf(term)
+        return tf * idf
+
+    def __add_document(self, doc_id: int, text: str):
+        tokens = tokenize_text(text)
+        for token in tokens:
+            self.index.setdefault(token, set()).add(doc_id)
+        self.term_frequencies[doc_id].update(tokens)
+
+def build_command() -> None:
+    idx = InvertedIndex()
+    idx.build()
+    idx.save()
+
 def search_command(query: str, limit: int = DEFAULT_SEARCH_LIMIT) -> list[dict]:
-    movies = load_movies()
-    results = []
-    for movie in movies:
-        query_tokens = tokenize_text(query)
-        title_tokens = tokenize_text(movie["title"])
-        if has_matching_token(query_tokens, title_tokens):
-            results.append(movie)
+    idx = InvertedIndex()
+    idx.load()
+    query_tokens = tokenize_text(query)
+    seen, results = set(), []
+    for query_token in query_tokens:
+        matching_doc_ids = idx.get_documents(query_token)
+        for doc_id in matching_doc_ids:
+            if doc_id in seen:
+                continue
+            seen.add(doc_id)
+            doc = idx.docmap[doc_id]
+            results.append(doc)
             if len(results) >= limit:
-                break
+                return results
     return results
 
+def tf_command(doc_id: int, term: str) -> int:
+    idx = InvertedIndex()
+    idx.load()
+    return idx.get_tf(doc_id, term)
+
+def idf_command(term: str) -> float:
+    idx = InvertedIndex()
+    idx.load()
+    return idx.get_idf(term)
+
+def tfidf_command(doc_id: int, terms: str) -> float:
+    idx = InvertedIndex()
+    idx.load()
+    return idx.get_tf_idf(doc_id, terms)
 
 def has_matching_token(query_tokens: list[str], title_tokens: list[str]) -> bool:
     for query_token in query_tokens:
